cd58f4cdaa7b5ef2c56e3cd8f7875347caf14ae8
jonathan roger <jroger@a5sys.com>
2017-10-03 13:27:29 +0200

    Adding JEST

---
+++ src/api/531Engine.test.ts
@@ -0,0 +1,11 @@
+/**
+ * Engine - Allow a user to know his program from 4 performances to the main lifts
+ */
+
+let sum = (a:number, b:number ) => {
+    return a + b;
+} 
+
+test('adds 1 + 2 to equal 3', () => {
+    expect(sum(1, 2)).toBe(3); 
+});
